664d22401565bf42e2872bb652cf0cee8e1e9e15
Sid Ahrend <sidahrend57@gmail.com>
2019-11-20 11:26:50 -0700

    added seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,30 @@
-select * from bullshit
+create table users(
+user_id serial primary key, 
+email varchar(75), 
+password varchar(250),
+username varchar(50),
+profile_pic text,
+cover_pic text, 
+real_name varchar(75),
+contact text, 
+bio text
+);
+
+create table posts(
+post_id serial primary key,
+user_id int references users(user_id),
+media text,
+title varchar(75),
+description varchar(250), 
+tags varchar(100), 
+views int, 
+saves boolean
+);
+
+create table comments (
+comment_id serial primary key,
+user_id int references users(user_id),
+post_id int references posts(post_id),
+comment text
+);
+
