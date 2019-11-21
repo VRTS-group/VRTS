6a27bdd4b8d93edf74562e8b08a0054587957606
trcasper <trcasper90@gmail.com>
2019-11-21 10:15:52 -0700

    css on settings and profile

--- src/Component/Settings.js
+++ src/Component/Settings.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
+import './Settings.css';
 
 class Settings extends Component {
   render() {
-    return <div>Settings</div>;
+    return <div className="Settings">
+      <div className="SettingsContainer">
+        <div className="SettingsTitle">Settings</div>
+        <div className="SettingsCover">
+          <div className="SettingsPic"></div>
+        </div>
+        <div className="SettingsInfo">
+          <h4>Profile Pic</h4>
+          <input
+          placeholder="Your Profile Pic"/>
+          <h4>Cover Photo</h4>
+          <input
+          placeholder="Your Cover Photo"/>
+          <h4>Username</h4>
+          <input
+          placeholder="Your Username"/>
+          <h4>Real Name</h4>
+          <input
+          placeholder="Your Real Name"/>
+          <h4>Bio</h4>
+          <textarea
+          placeholder="Your Bio"/>
+        </div>
+        <div className="SettingsBtns">
+        <button>Save</button>
+        </div>
+        {/* //end */}
+      </div>
+    </div>;
   }
 }
 
