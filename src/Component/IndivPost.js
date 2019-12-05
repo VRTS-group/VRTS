ab6b4f2ae837c9ac5b8e339340dff6939f86ab64
valAizcorbe <valeria.aizcorbe@hotmail.com>
2019-12-05 13:34:02 -0700

    alerts, and more

--- src/Component/IndivPost.js
+++ src/Component/IndivPost.js
@@ -4,8 +4,8 @@ import { deleteComment } from "../redux/commentReducer";
 import axios from "axios";
 import "./IndivPost.css";
 import { updateUser, getUserById } from "../redux/userReducer";
-import { Link } from 'react-router-dom';
- 
+import { Link } from "react-router-dom";
+
 class IndivPost extends Component {
   constructor(props) {
     super(props);
@@ -33,7 +33,7 @@ class IndivPost extends Component {
         user_id: res.data[0].user_id,
         post_id: res.data[0].post_id
       });
-      console.log(res.data);
+      // console.log(res.data);
     });
     axios.get(`/api/comments/${this.props.match.params.id}`).then(res => {
       if (res.data[0]) {
@@ -44,13 +44,13 @@ class IndivPost extends Component {
           comments: res.data
         });
       }
-      console.log(res.data);
+      // console.log(res.data);
     });
     axios.get(`/auth/getUserById/${this.props.match.params.id}`).then(res => {
       this.setState({
         user: res.data
       });
-      console.log(res.data);
+      // console.log(res.data);
     });
     this.props.getUserById();
   };
@@ -84,7 +84,7 @@ class IndivPost extends Component {
         alert("Saved!");
       })
       .catch(err => console.log(err));
-    console.log(this.state.saves);
+    // console.log(this.state.saves);
   };
 
   newComment = () => {
@@ -109,123 +109,135 @@ class IndivPost extends Component {
 
   render() {
     let { comment } = this.state;
-    console.log(this.state)
+    // console.log(this.state)
     // console.log(this.state.username);
     // console.log(this.state.username);
-    console.log(this.state.posts);
+    // console.log(this.state.posts);
     // console.log(this.state.user);
     // console.log(this.props.redux.userReducer.user.username);
     // console.log(this.props);
     return (
-      <div id='indiv-post-page'>
-      <section className="indiv-post">
-        <div className="username">
-          {this.state.posts.map(e => {
-            return <Link to={`/profile/${e.user_id}`}><h3> {e.username} </h3></Link>;
-          })}
-        </div>
-        <div className="post-section ">
-          {this.state.posts.map(e => {
-            return (
-              <div id="post-info">
-                <div id="title-box">
-                  <h2>{e.title}</h2>
-                </div>
-                <div id="imagen-button">
-                  <img src={e.media} className="post-picture" />
-                  <button
-                    onClick={() => this.handleSave()}
-                    className="button-on-top"
-                  >
-                    Save
-                  </button>
-                </div>
-                <div className="description">
-                  <h3>{e.description}</h3>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        
-        <section id="comment-section">
-          <div id='comment-bar'>
-          <textarea
-            className="text-box"
-            name="comment"
-            value={comment}
-            placeholder="Add a comment here :)"
-            onChange={this.handleText}
-          ></textarea>
-          <br />
-          <div id='baby-button-box'>
-            <button onClick={this.newComment} className="button-indiv">
-              Write a comment
-            </button>
-            <br />
-            <button onClick={this.clear} className="button-indiv">
-              Clear
-            </button>
-          </div>
-          </div>
-          <div className="potato">
+      <div id="indiv-post-page">
+        <section className="indiv-post">
+          <div className="username">
             {this.state.posts.map(e => {
               return (
-                <div>
-                  {e.user_id === this.props.redux.userReducer.user.user_id ? (
-                    <div>
-                      {this.state.comments.map(e => {
-                        return (
-                          <div className="comment-area">
-                            
-                            {/* <img
-                              src={e.profile_pic}
-                              className="profile-comment"
-                            /> */}
-
-                              <h5>{e.username}</h5>
-                              {e.comment}
-                            <button
-                              className="delete-button-indiv"
-                              onClick={() => {
-                                deleteComment(e.comment_id);
-                              }}
-                              >
-                              Delete
-                            </button>
-                              
-                          </div>
-                        );
-                      })}
-                    </div>
-                  ) : (
-                    <div>
-                      {this.state.comments.map(e => {
-                        return (
-                          <div className="comment-user">
-                            <img
-                              src={e.profile_pic}
-                              className="profile-comment"
-                            />
-
-                            <div className="comment-area">
-                              <h5>{e.username}</h5>
-                                  {e.comment}
-                            </div>
-                          </div>
-                        );
-                      })}
-                    </div>
-                  )}
+                <Link to={`/profile/${e.user_id}`}>
+                  <h3> {e.username} </h3>
+                </Link>
+              );
+            })}
+          </div>
+          <div className="post-section ">
+            {this.state.posts.map(e => {
+              return (
+                <div id="post-info">
+                  <div id="title-box">
+                    <h2>{e.title}</h2>
+                  </div>
+                  <div id="imagen-button">
+                    <img src={e.media} className="post-picture" />
+                    <button
+                      onClick={() => this.handleSave()}
+                      className="button-on-top"
+                    >
+                      ♡
+                    </button>
+                  </div>
+                  <div className="description">
+                    <h3>{e.description}</h3>
+                  </div>
                 </div>
               );
             })}
           </div>
 
-          {/* <button>Show more</button> */}
+          <section id="comment-section">
+            <div id="comment-bar">
+              <textarea
+                className="text-box"
+                name="comment"
+                value={comment}
+                placeholder="Add a comment here :)"
+                onChange={this.handleText}
+              ></textarea>
+              <br />
+              <div id="baby-button-box">
+                <button onClick={this.newComment} className="button-indiv">
+                  Post
+                </button>
+                <br />
+                <button onClick={this.clear} className="button-indiv">
+                  Clear
+                </button>
+              </div>
+            </div>
+            <div className="potato">
+              {this.state.posts.map(e => {
+                return (
+                  <div>
+                    {e.user_id === this.props.redux.userReducer.user.user_id ? (
+                      <div>
+                        {this.state.comments.map(e => {
+                          return (
+                            <div className="comment-user">
+                              <Link to={`/profile/${e.user_id}`}>
+                                <img
+                                  src={e.profile_pic}
+                                  className="profile-comment"
+                                />
+                              </Link>
+
+                              <div className="comment-area">
+                                <Link to={`/profile/${e.user_id}`}>
+                                  <h5>{e.username}</h5>
+                                </Link>
+                                {e.comment}
+                              </div>
+                              <button
+                                className="delete-button-indiv"
+                                onClick={() => {
+                                  deleteComment(e.comment_id);
+                                }}
+                              >
+                                ✖
+                              </button>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    ) : (
+                      <div>
+                        {this.state.comments.map(e => {
+                          return (
+                            <div className="comment-user">
+                              <Link to={`/profile/${e.user_id}`}>
+                                <img
+                                  src={e.profile_pic}
+                                  className="profile-comment"
+                                />
+                              </Link>
+
+                              <div className="comment-area">
+                                <Link to={`/profile/${e.user_id}`}>
+                                  <h5>{e.username}</h5>
+                                </Link>
+                                {e.comment}
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+
+            {/* <button>Show more</button> */}
+          </section>
         </section>
-      </section>
-    </div>
+      </div>
     );
   }
 }
