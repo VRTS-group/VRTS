94ba9da864cb4df8e27148109f85a769188b79e4
Sid Ahrend <sidahrend57@gmail.com>
2019-12-05 23:28:44 -0700

    fingers crossed

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const WriteCTRL = require("./writeController");
 const auth = require("./authmiddleware");
 const app = express();
 
-// app.use(express.json());
-app.use( express.static( `${__dirname}/../build` ) );
+app.use(express.json());
+// app.use( express.static( `${__dirname}/../build` ) );
 
 massive(CONNECTION_STRING).then(db => {
   app.set("db", db);
